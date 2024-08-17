e04767fd4ada0067f65805a08c6a82cf7bdc9aca
Nebula <hscloud913@naver.com>
2024-08-17 23:04:52 +0900

    프로젝트 기본 환경 설정 적용
    - reset css 적용, 기본 폰트 적용
    - light, dark 모드 전환

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Header from './components/header/Header.tsx'
+import './style.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App: React.FC = () => {
+    const [theme, setTheme] = useState<'light' | 'dark'>(() => {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    });
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    }
+
+    return (
+        <>
+
+            <div>
+                <button onClick={toggleTheme}>
+                    Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+                </button>
+            </div>
+
+        </>
+    )
 }
 
 export default App
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <div>
+
+                </div>
+
+                <nav>
+                    <ul>
+                        <li><a href="#intro">Intro</a></li>
+                        <li><a href="#intro">Skill</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
