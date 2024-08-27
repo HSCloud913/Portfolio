c9e3fe27980b63893d131d8c1fe4fcf375dffea4
Nebula <hscloud913@naver.com>
2024-08-28 00:10:41 +0900

    Fix : Career page가 잘못 routing 되어있던 현상 수정

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,7 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import styled from 'styled-components';
 import Home from '@pages/Home/Home.tsx'
 import About from '@pages/About/About.tsx'
-import Carrer from '@pages/Career/Career.tsx'
+import Career from '@pages/Career/Career.tsx'
 import Skills from '@pages/Skills/Skills.tsx'
 import Project from '@pages/Project/Project.tsx'
 import Contact from '@pages/Contact/Contact.tsx'
@@ -19,7 +19,7 @@ const Main: React.FC = () => {
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/about" element={<About/>}/>
-                    <Route path="/carrer" element={<Carrer/>}/>
+                    <Route path="/career" element={<Career/>}/>
                     <Route path="/skills" element={<Skills/>}/>
                     <Route path="/project" element={<Project/>}/>
                     <Route path="/contact" element={<Contact/>}/>
